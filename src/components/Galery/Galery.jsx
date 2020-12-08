f47f48a4e1575c24f9eb808891dc2f4ab8e2814b
Artem <unnamed_9595@mail.ru>
2020-12-08 15:16:03 +0300

    fix warnings 1

--- src/components/Galery/Galery.jsx
+++ src/components/Galery/Galery.jsx
@@ -5,13 +5,15 @@ import Renderer3D from './Renderer3D';
 import './Galery.css'
 
 const Galery = () => {
+  // eslint-disable-next-line
   const galeryRef = React.useRef(null);
+  // eslint-disable-next-line
   let galeryClass = React.useRef(null);
 
   React.useEffect(() => {
+    // eslint-disable-next-line
     galeryClass = new Renderer3D(galeryRef.current);
-    console.log(galeryClass);
-  }, [galeryRef])
+  }, [galeryRef, galeryClass])
   return <div className="root" ref={galeryRef} />
 };
 
