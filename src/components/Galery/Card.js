36eddcff2eaaf9647505fc153030a87a9ee9212c
Artem <unnamed_9595@mail.ru>
2020-12-08 15:06:40 +0300

    add infinity scroll

--- src/components/Galery/Card.js
+++ src/components/Galery/Card.js
@@ -1,151 +1,96 @@
-import * as THREE from 'three';
-import SimplexNoise from 'simplex-noise';
-// import { FaceNormalsHelper } from 'three/examples/jsm/helpers/FaceNormalsHelper.js';
+import * as PIXI from 'pixi.js';
+
+const jsImageCover = (image, innerWidth, innerHeight) => {
+  if (!innerHeight) {
+    const widthK = innerWidth / image.width;
+    return {
+      width: image.width * widthK,
+      height: image.height * widthK,
+    };
+  }
+  const screenAspect = innerWidth / innerHeight;
+  const imageAspect = image.width / image.height;
+  if (screenAspect > imageAspect) {
+    return {
+      width: innerWidth,
+      height: (innerWidth / image.width) * image.height,
+    };
+  }
+  return {
+    width: (innerHeight / image.height) * image.width,
+    height: innerHeight,
+  };
+};
 
 
-export default class Card {
-  constructor(pos, size, texture) {
-    this.pos = pos;
+export default class CoverImage {
+  constructor(sprite, imageSettings) {
+    this.imageSettings = imageSettings;
+    this.sprite = sprite;
 
-    this.width = size.width;
-    this.height = size.height;
-    this.texture = texture;
-    this.simplex = new SimplexNoise();
-    const value2d = this.simplex.noise2D(this.pos.x, this.pos.y);
+    this.mask = new PIXI.Graphics();
 
-    // console.log(this.texture);
+    this.sprite.mask = this.mask;
+    this.sprite.anchor.set(0.5);
 
-    this.friction = THREE.MathUtils.lerp(Math.abs(value2d), 0.25, 0.8); //THREE.MathUtils.randFloat(0.15, 0.5);
-    const { x, y } = this.getUvRate(this.texture.image, this.width, this.height);
-    this.uvRate = new THREE.Vector2(x, y);
-
-    this.geometry = new THREE.PlaneGeometry(this.width, this.height, 32 );
-    // this.material = new THREE.MeshPhongMaterial({ color: this.color || 0xff0000 } );
-    this.material = new THREE.ShaderMaterial({
-      uniforms: {
-        uTexture: {
-          value: this.texture,
-        },
-        uvRate: {
-          type: 'v2',
-          value: this.uvRate,
-        },
-        pixeles: {type: 'v2', value: new THREE.Vector2(this.width, this.height)},
-      },
-      vertexShader: `
-      precision highp float;
-
-      uniform vec2 uvRate;
-
-      varying vec2 vUv;
-      
-      void main() {
-        vUv = uv;
-        vUv -= 0.5;
-        vUv *= uvRate;
-        vUv += 0.5;
-
-        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0 );
-      }
-      `,
-      fragmentShader: `
-      precision highp float;
-
-      uniform sampler2D uTexture;
-      uniform vec2 pixeles;
-      uniform vec2 uvRate;
-
-      varying float vFrontShadow;
-      varying vec2 vUv;
-      
-      void main()	{
-        vec2 uv = gl_FragCoord.xy / pixeles.xy;
-        vec4 tex1 = texture2D(uTexture ,vUv);
-        gl_FragColor = tex1;
-        // gl_FragColor = vec4(1., 0., 0., 1.);
-      }
-      `,
-    });
-    this.plane = new THREE.Mesh(this.geometry, this.material);
-    // this.helper = new FaceNormalsHelper( this.plane, 2, 0x00ff00, 1 );
-    this.plane.position.set(this.pos.x, this.pos.y, this.pos.z);
-    this.raycaster = new THREE.Raycaster(this.plane.position);
-    this.arrowHelper = new THREE.ArrowHelper(this.raycaster.ray.direction, this.plane.position, 50, 0x000000 );
+    this.onResize(imageSettings);
   }
 
-  getUvRate = (image, width, height) => {
-    const wrapperAspect = width / height;
-    const imageAspect = image.width / image.height;
-  
-    if (wrapperAspect > imageAspect) {
-      const sizes = {
-        width,
-        height: (width / image.width) * image.height,
-      };
-      return { x: 1, y: (height / sizes.height) / (width / sizes.width) };
+  getPositions = () => ({
+    x: this.imageSettings.x || 0,
+    y: this.imageSettings.y || 0,
+  })
+
+  getSize = () => ({
+    width: this.imageSettings.width || 0,
+    height: this.imageSettings.height || 0,
+  })
+
+  getType = () => (
+    this.imageSettings.type || 'cover'
+  )
+
+  getCurrentImageSize = () => {
+    const { width, height } =  this.getSize();
+    if (this.sprite.texture) {
+      return jsImageCover(this.sprite.texture, width, height);
     }
-    const sizes = {
-      width: (height / image.height) * image.width,
+
+    return {
+      width,
       height,
     };
-    const x = (width / sizes.width) / (height / sizes.height);
-    return {x, y: 1};
-  };
-
-  setTargetPlane = (plane) => {
-    this.rayTarget = plane;
   }
 
-  // map = (in_min, in_max, out_min, out_max) => {
-  //   return ((this - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
-  //  }
+  renderMask = () => {
+    const { x, y } = this.getPositions();
+    const { width, height } = this.getSize();
 
-  getCard = () => this.plane;
-
-  getHelper = () => this.helper;
-
-  animate = () => {
-    
+    this.mask.clear();
+    this.mask.beginFill(0x8bc5ff);
+    this.mask.drawRect(x, y, width, height);
+    this.mask.endFill();
   }
 
-  moveY = (delta) => {
-    // let currentContainerPOsition = this.plane.position.y;
-    // currentContainerPOsition += (delta - currentContainerPOsition) * this.friction;
+  renderImage = () => {
+    const { width: originW, height: originH } = this.getSize();
+    const { width, height } =  this.getCurrentImageSize();
+    const { x, y } = this.getPositions();
 
-    // this.pos.y = currentContainerPOsition;
-    this.plane.position.y += delta;
+    this.sprite.x = x + (originW / 2);
+    this.sprite.y = y + (originH / 2);
+    this.sprite.width = width;
+    this.sprite.height = height;
   }
 
-  update = (pos) => {
-    let currentContainerPOsitionZ = this.plane.position.z;
-    currentContainerPOsitionZ += (pos.z - currentContainerPOsitionZ) * this.friction;
-    this.pos.z = currentContainerPOsitionZ;
-    this.plane.position.z = this.pos.z;
-
-    let currentContainerPOsitionY = this.plane.position.y;
-    currentContainerPOsitionY += (this.pos.y - currentContainerPOsitionY) * this.friction;
-    this.plane.position.y = currentContainerPOsitionY;
-
-    let currentContainerPOsitionX = this.plane.position.x;
-    currentContainerPOsitionX += (this.pos.x - currentContainerPOsitionX) * this.friction;
-    this.plane.position.x = currentContainerPOsitionX;
-    // const intersects = this.raycaster.intersectObjects([this.rayTarget]);
-
-    // for ( let i = 0; i < intersects.length; i ++ ) {
-
-    //   console.log(intersects[ i ]);
-  
-    // }
-
-
-    // let currentContainerPOsitionY = this.plane.position.y;
-    // currentContainerPOsitionY += (pos.y - currentContainerPOsitionY) * this.friction;
-
-    // this.pos.y = currentContainerPOsitionY;
-    // this.plane.position.y = this.pos.y;
+  setTexture = texture => {
+    this.sprite.texture = texture;
+    this.renderImage();
   }
 
-  render = () => {
-
+  onResize = (imageSettings) => {
+    this.imageSettings = imageSettings;
+    this.renderImage();
+    this.renderMask();
   }
 }
--- src/components/Galery/Renderer3D.js
+++ src/components/Galery/Renderer3D.js
@@ -1,16 +1,10 @@
 /* eslint-disable */
-import * as THREE from 'three';
+import * as PIXI from 'pixi.js';
 import normalizeWheel from 'normalize-wheel';
-import {EffectComposer} from 'three/examples/jsm/postprocessing/EffectComposer.js';
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
-import {RenderPass} from 'three/examples/jsm/postprocessing/RenderPass.js'
-// import {FilmPass} from 'three/examples/jsm/postprocessing/FilmPass.js';
-import {ZoomShader} from './zoomShader';
-import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js';
-import gsap from 'gsap';
+// import gsap from 'gsap';
 import {hotGirls} from './hotGirls';
 
-import Card from './Card';
+// import CoverImage from './Card';
 
 const map = (num, in_min, in_max, out_min, out_max) => {
   return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
@@ -19,265 +13,132 @@ const map = (num, in_min, in_max, out_min, out_max) => {
 export default class Renderer3D {
   constructor(dom) {
     this.dom = dom;
+    this.loadingScreenLoader = new PIXI.Loader();
     this.textures = [];
 
-    this.scale = 1;
+    this.rw = 0;
+    this.rh = 0;
 
-    this.isZoomBehaviour = true;
+    this.app = new PIXI.Application({
+      width: window.innerWidth,
+      height: window.innerHeight
+    });
 
-    this.currentScrollZ = 0;
+    this.friction = 0.5;
+
+    this.dom.addEventListener('wheel', this.handleWheel);
+
+    this.scrolltargetX = 0;
+    this.scrolltargetY = 0;
     this.currentScrollX = 0;
     this.currentScrollY = 0;
 
-    this.direction = {
-      x: null,
-      y: null,
-      dx: 0,
-      dy: 0,
+    this.cardWidth = 150;
+    this.cardHeight = 200;
+
+    this.countCardInRow = 12;
+    this.countCardInCol = 13;
+
+    this.wholewidth = this.countCardInRow * (this.cardWidth + 50);
+    this.wholeheight = this.countCardInCol * (this.cardHeight + 50);
+    this.dom.appendChild(this.app.view);
+    
+    this.container = new PIXI.Container();
+    this.rectangleContainer = new PIXI.Container();
+    
+    this.app.stage.addChild(this.container);
+    this.app.stage.addChild(this.rectangleContainer);
+
+    this.app.renderer.backgroundColor = 0xFFFfFF;
+    this.preload();
+  }
+
+  randomInt = (min, max) => {
+    return min + Math.floor((max - min) * Math.random());
+  }
+
+  createGrid = (w, h) => {
+    const grid = [];
+    for (let coll = 0; coll < w; coll++) {
+      const tempRow = [];
+      const x = coll * (this.cardWidth + 50);
+      for (let row = 0; row < h; row++) {
+        let y = row * this.cardHeight + row * 50;
+        if (coll % 2 === 0) {
+          y+= (this.cardHeight + 50) / 2;
+        }
+        const texture = PIXI.Texture.from(hotGirls[this.randomInt(0, hotGirls.length - 1)]);
+        const bunny = new PIXI.Sprite(texture);
+        bunny.width = this.cardWidth;
+        bunny.height = this.cardHeight;
+        bunny.x = x;
+        bunny.y = y;
+        // bunny.anchor.set(0.5);
+        tempRow.push(bunny);
+        this.container.addChild(bunny);
+      }
+      grid.push(tempRow);
     }
-
-    this.allCards = [];
-
-    this.currentPageScrollY = 0;
-
-    this.mouse = new THREE.Vector3(0, 0, 0)
-    
-    // Границы
-    this.farPos = -1;
-    this.nearPos = 3;
-    
-    this.finalPos = this.farPos;
-
-    this.scene = new THREE.Scene();
-    this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 2000 );
-    this.camera.position.z = 5;
-    
-
-    this.renderer = new THREE.WebGLRenderer();
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-    // this.renderer.setClearColorHex( 0xffffff, 1 );
-
-    this.composer = new EffectComposer(this.renderer);
-    this.renderPass = new RenderPass(this.scene, this.camera);
-    this.composer.addPass(this.renderPass);
-
-    this.colorPass = new ShaderPass(ZoomShader);
-    this.colorPass.renderToScreen = true;
-    this.composer.addPass(this.colorPass);
-    dom.appendChild(this.renderer.domElement);
-
-    // Координаты
-    this.pos = new THREE.Vector3(0, 0, this.farPos);
+    return grid;
+  }
 
 
-    this.light = new THREE.AmbientLight( 0xffffff );
-    this.scene.add(this.light);
-    this.cardsGroup = new THREE.Group();
-    this.scene.add(this.cardsGroup);
-    this.scene.background = new THREE.Color( 0xffffff );
-
-    this.cards = [];
-
-    this.collsCount = 10;
-    this.rowsCount = 5;
-
-    this.vMargn = 1;
-    this.hMargn = 1;
-
-    this.cardHeight = 3;
-    this.cardWidth = 2;
-
-    this.vOffset = (this.cardHeight + this.vMargn) / 2;
-
-    const planeWidth = (this.cardWidth + this.vMargn) * this.collsCount;
-    const planeHeight = (this.cardHeight + this.hMargn) * this.rowsCount + this.vOffset * 2;
-
-    this.geometry = new THREE.PlaneGeometry(planeWidth, planeHeight, 32 );
-    this.material = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
-    this.plane = new THREE.Mesh(this.geometry, this.material);
-    // this.scene.add(this.plane);
-
-    this.controls = new OrbitControls( this.camera, this.renderer.domElement );
-
-    const loadManager = new THREE.LoadingManager();
-    const loader = new THREE.TextureLoader(loadManager);
-    hotGirls.forEach(url => {
-      this.textures.push(loader.load(url));
+  preload = () => {
+    hotGirls.forEach(item => {
+      this.loadingScreenLoader.add(item, item);
     });
 
-    loadManager.onLoad = () => {
-      const imgsCount = this.collsCount * this.rowsCount;
-      let coll = [];
-      for (let i = 0; i < imgsCount; i += 1) {
-        const card = new Card(
-          new THREE.Vector3(0, 0, this.pos.z),
-          {
-            width: this.cardWidth,
-            height: this.cardHeight
-          },
-          this.textures[THREE.MathUtils.randInt(0, this.textures.length - 1)]
-        );
-        this.allCards.push(card);
-        coll.push(card)
-        // console.log(i % this.rowsCount);
-        if (i % this.rowsCount === this.rowsCount - 1) {
-          const collGroup = this.buildCol(coll);
-          this.cardsGroup.add(collGroup);
-          this.cards.push(collGroup);
-          coll = [];
-        }
-        // this.scene.add(card.getCard());
-      }
-      this.setPosToColls(this.cards);
-      this.setGroupOffsetToCenter();
-    };
-    // this.cardsGroup.computeBoundingBox();
-    console.log(this.cardsGroup);
-    this.dom.addEventListener('wheel', this.handleWheel);
-    this.dom.addEventListener('mousedown', this.handleMouseDown);
-    this.dom.addEventListener('mouseup', this.handleMouseUp);
+    this.loadingScreenLoader.load();
+    this.loadingScreenLoader.onComplete.add(() => {
+      this.createGrid(this.countCardInRow, this.countCardInCol);
+      // const graphics = new PIXI.Graphics();
 
+      // Rectangle
+      // this.rw = this.container.width;
+      // this.rh = this.container.height;
+      // graphics.beginFill(0xDE3249, 0.25);
+      // graphics.drawRect(0, 0, this.rw, this.rh);
+      // graphics.endFill();
+      // this.rectangleContainer.addChild(graphics);
     
     this.render();
-  }
-
-  buildCol = (images) => {
-    const coll = new THREE.Group();
-    images.forEach((c, i) => {
-      let posY = i * (this.cardHeight + this.vMargn);
-      c.plane.position.y = posY;
-      coll.add(c.getCard());
-    })
-    return coll;
-  }
-
-  setPosToColls = (groups) => {
-    groups.forEach((g, i) => {
-      const posX = i * (this.cardWidth + this.hMargn);
-      g.position.x = posX;
-      if (i % 2 === 0) {
-        g.position.y += this.vOffset
-      }
     });
-  }
+  };
 
-  setGroupOffsetToCenter = () => {
-    this.cardsGroup.position.x = -(this.collsCount - 1) * (this.cardWidth + this.hMargn) / 2;
-    this.cardsGroup.position.y = -(this.rowsCount - 1) * (this.cardHeight + this.vMargn) / 2;;
-  }
+
 
   handleWheel = (e) => {
     const normalized = normalizeWheel(e);
     const { pixelY, pixelX } = normalized;
-    if (this.isZoomBehaviour) {
-      this.currentScrollZ = THREE.MathUtils.clamp(this.currentScrollZ + pixelY, this.farPos, this.nearPos);
-      const finalPos = THREE.MathUtils.lerp(this.farPos, this.nearPos, this.currentScrollZ / 250);
-      
-      gsap.to(this, {
-        duration: 0.5,
-        finalPos,
-        overwrite: 5,
-      })
-    } else {
-      this.allCards.forEach(c => {
-        gsap.to(c.plane.position, {
-          duration: 0.5,
-          y: c.plane.position.y + pixelY,
-          x: c.plane.position.x + -pixelX,
-          overwrite: 5,
-        })
-      })
-    }
-  }
 
-  handleMouseDown = (e) => {
-    this.isDown = true;
-    const { width, height } = this.dom.getBoundingClientRect();
-    this.mouse.x = (event.clientX / width) * 2 - 1;
-    this.mouse.y = -(event.clientY / height) * 2 + 1;
-    gsap.to(this, {
-      duration: 0.5,
-      scale: 0.75,
-    })
-    gsap.to(this.colorPass.uniforms.uZoom, {
-      duration: 0.5,
-      value: 0.5,
-    })
-    this.dom.addEventListener('mousemove', this.handleMouseMove);
-  }
+    this.scrolltargetX = pixelX;
+    this.scrolltargetY = pixelY;
 
-  handleMouseUp = () => {
-    this.isDown = false;
-    gsap.to(this.colorPass.uniforms.uZoom, {
-      duration: 0.5,
-      value: 0,
-    })
-    gsap.to(this, {
-      duration: 0.5,
-      scale: 1,
+    this.container.children.forEach(s => {
     });
-    this.direction.x = null;
-    this.direction.y = null;
-    this.direction.dx = 0;
-    this.direction.dy = 0;
-    this.dom.removeEventListener('mousemove', this.handleMouseMove);
   }
 
   handleMouseMove = (event) => {
-    const { width, height } = this.dom.getBoundingClientRect();
-    const mouse = new THREE.Vector2();
-    mouse.x = (event.clientX / width) * 2 - 1;
-    mouse.y = -(event.clientY / height) * 2 + 1;
 
-    const dx = (this.mouse.x - mouse.x) * 250;
-    const dy = (this.mouse.y - mouse.y) * 250;
-    this.currentScrollX += dx;
-    this.currentScrollY += dy;
-    this.allCards.forEach(c => {
-      c.pos.x += dx;
-      c.pos.y += dy;
-    });
-
-    this.direction.x = dx < 0 ? 'right' : 'left';
-    this.direction.y = dy < 0 ? 'up' : 'down';
-    this.direction.dx = dx;
-    this.direction.dy = dy;
-    if (dx === 0) {
-      this.direction.x = null;
-    }
-
-    if (dy === 0) {
-      this.direction.y = null;
-    }
-    this.mouse = mouse;
   }
 
-  animate = () => {
-    
-  }
+  calcPos = (scroll, pos, whole, size) => {
 
-  update = () => {
-    // console.log(this.currentPageScrollY);
-    this.scale = THREE.MathUtils.clamp(this.colorPass.uniforms.uZoom.value, 0, 0.25);
-    // this.allCards.forEach(c => {
-    //   c.update(this.pos);
-    //   c.plane.scale.x = this.scale;
-    //   c.plane.scale.y = this.scale;
-    //   c.plane.scale.z = this.scale;
-    // })
-    this.pos.z = this.finalPos;
-    this.pos.x = this.currentScrollX;
-    this.pos.y = this.currentScrollY;
-    
+    let temp = (scroll + pos + whole + size + 50) % whole - size - 50;
+
+    return temp;
   }
 
   render = () => {
-    // console.log(this.direction);
-    this.animate();
-    this.update();
-    this.controls.update();
-    requestAnimationFrame(this.render);
-    // this.renderer.render(this.scene, this.camera);
-    this.composer.render();
+    this.app.ticker.add(() => {
+      if (this.scrolltargetX === 1 || this.scrolltargetX === -1) { this.scrolltargetX = 0; }
+      if (this.scrolltargetY === 1 || this.scrolltargetY === -1) { this.scrolltargetY = 0; }
+      this.currentScrollX -= (this.currentScrollX - this.scrolltargetX) * 0.1;
+      this.currentScrollY -= (this.currentScrollY - this.scrolltargetY) * 0.1;
+      this.container.children.forEach(s => {
+        s.position.x = this.calcPos(this.currentScrollX, s.position.x, this.wholewidth, this.cardWidth);
+        s.position.y = this.calcPos(this.currentScrollY, s.position.y,this.wholeheight, this.cardHeight);
+      });
+  });
   }
 }
