f786662b5ea48bf6078598577f232f549aafda99
Artem <unnamed_9595@mail.ru>
2020-12-09 15:58:59 +0300

    add wheel scale

--- src/components/Galery/Renderer3D.js
+++ src/components/Galery/Renderer3D.js
@@ -37,14 +37,20 @@ export default class Renderer3D {
     this.currentScrollX = 0;
     this.currentScrollY = 0;
 
-    this.cardWidth = 150;
-    this.cardHeight = 200;
+    this.scale = 0.9;
+    this.oldscale = this.scale;
+
+    this.cardWidth = 250;
+    this.cardHeight = 350;
 
     this.countCardInRow = 12;
     this.countCardInCol = 13;
 
-    this.wholewidth = this.countCardInRow * (this.cardWidth + 50);
-    this.wholeheight = this.countCardInCol * (this.cardHeight + 50);
+    this.vMargin = 80;
+    this.hMargin = 80;
+
+    this.wholewidth = this.countCardInRow * (this.cardWidth + this.vMargin);
+    this.wholeheight = this.countCardInCol * (this.cardHeight + this.hMargin);
     this.dom.appendChild(this.app.view);
     
     this.container = new PIXI.Container();
@@ -52,14 +58,13 @@ export default class Renderer3D {
     this.bgFilter = new BulgePinchFilter()
     this.container.filters = [this.bgFilter];
     
-
-    console.log(this.bgFilter.uniforms);
     this.bgFilter.uniforms.radius = Math.max(window.innerWidth, window.innerHeight);
     this.bgFilter.uniforms.strength = 0;
     
     this.app.stage.addChild(this.container);
     this.app.stage.addChild(this.rectangleContainer);
 
+    this.dom.addEventListener('wheel', this.handleWheel);
     this.dom.addEventListener('mousedown', this.handleMouseDown);
     this.dom.addEventListener('mouseup', this.handleMouseUp);
     window.addEventListener('mouseover', this.handleMouseUp);
@@ -71,10 +76,11 @@ export default class Renderer3D {
   }
 
   handleMouseMove = (e) => {
+    console.log('move');
     const { width, height } = this.dom.getBoundingClientRect();
     const mouse = { x: 0, y: 0 };
-    mouse.x = (event.clientX / width) * 2 - 1;
-    mouse.y = -(event.clientY / height) * 2 + 1;
+    mouse.x = (e.clientX / width) * 2 - 1;
+    mouse.y = -(e.clientY / height) * 2 + 1;
 
     const dx = -(this.mouse.x - mouse.x) * 250;
     const dy = (this.mouse.y - mouse.y) * 250;
@@ -88,20 +94,26 @@ export default class Renderer3D {
     console.log('down');
     this.isDown = true;
     const { width, height } = this.dom.getBoundingClientRect();
-    this.mouse.x = (event.clientX / width) * 2 - 1;
-    this.mouse.y = -(event.clientY / height) * 2 + 1;
+    this.mouse.x = (e.clientX / width) * 2 - 1;
+    this.mouse.y = -(e.clientY / height) * 2 + 1;
     gsap.to(this.bgFilter.uniforms, {
       duration: 0.5,
       strength: 0.5,
     })
+    this.oldscale = this.scale;
 
-    this.container.children.forEach(s => {
-      gsap.to(s.scale, {
-        duration: 0.5,
-        x: s.origScaleX * 0.8,
-        y: s.origScaleY * 0.8,
-      })
+    gsap.to(this, {
+      duration: 0.5,
+      scale: this.oldscale * 0.7,
     })
+
+    // this.container.children.forEach(s => {
+    //   gsap.to(s.scale, {
+    //     duration: 0.5,
+    //     x: s.origScaleX * this.scale * 0.5,
+    //     y: s.origScaleY * this.scale * 0.5,
+    //   })
+    // })
 
     window.addEventListener('mousemove', this.handleMouseMove);
   }
@@ -118,20 +130,19 @@ export default class Renderer3D {
       scrolltargetY: 0,
     });
 
-    this.container.children.forEach(s => {
-      gsap.to(s.scale, {
-        duration: 0.5,
-        x: s.origScaleX,
-        y: s.origScaleY,
-      })
+    gsap.to(this, {
+      duration: 0.5,
+      scale: this.oldscale,
+      // y: s.origScaleY * this.scale,
     })
-    // this.direction.x = null;
-    // this.direction.y = null;
-    // this.direction.dx = 0;
-    // this.direction.dy = 0;
 
-    // this.scrolltargetX = 0;
-    // this.scrolltargetY = 0;
+    // this.container.children.forEach(s => {
+    //   gsap.to(s.scale, {
+    //     duration: 0.5,
+    //     x: s.origScaleX * this.scale,
+    //     y: s.origScaleY * this.scale,
+    //   })
+    // })
     window.removeEventListener('mousemove', this.handleMouseMove);
   }
 
@@ -147,11 +158,11 @@ export default class Renderer3D {
     const grid = [];
     for (let coll = 0; coll < w; coll++) {
       const tempRow = [];
-      const x = coll * (this.cardWidth + 50);
+      const x = coll * (this.cardWidth + this.vMargin);
       for (let row = 0; row < h; row++) {
-        let y = row * this.cardHeight + row * 50;
+        let y = row * this.cardHeight + row * this.hMargin;
         if (coll % 2 === 0) {
-          y+= (this.cardHeight + 50) / 2;
+          y+= (this.cardHeight + this.hMargin) / 2;
         }
         const texture = PIXI.Texture.from(hotGirls[this.randomInt(0, hotGirls.length - 1)]);
         const bunny = new PIXI.Sprite(texture);
@@ -163,6 +174,10 @@ export default class Renderer3D {
         bunny.anchor.set(0.5);
         bunny.origScaleX = bunny.scale.x;
         bunny.origScaleY = bunny.scale.y;
+
+        bunny.scale.x = bunny.origScaleX * this.scale;
+        bunny.scale.y = bunny.origScaleY * this.scale;
+
         tempRow.push(bunny);
         this.container.addChild(bunny);
       }
@@ -198,18 +213,16 @@ export default class Renderer3D {
 
   handleWheel = (e) => {
     const normalized = normalizeWheel(e);
-    const { pixelY, pixelX } = normalized;
-
-    this.scrolltargetX = pixelX;
-    this.scrolltargetY = pixelY;
-
-    this.container.children.forEach(s => {
-    });
+    const { pixelY } = normalized;
+    gsap.to(this, {
+      scale: this.clamp(this.scale + pixelY / 100, 0.7, 1.1),
+      duration: 0.5,
+    })
   }
 
-  calcPos = (scroll, pos, whole, size) => {
+  calcPos = (scroll, pos, whole, size, margin) => {
 
-    let temp = (scroll + pos + whole + size + 50) % whole - size - 50;
+    let temp = (scroll + pos + whole + size + margin) % whole - size - margin;
 
     return temp;
   }
@@ -224,8 +237,10 @@ export default class Renderer3D {
       this.currentScrollY -= sy * 0.1;
       this.container.children.forEach(s => {
         // s.scale.set(s.scale.x + s.friction / 1000, s.scale.y + s.friction / 1000)
-        s.position.x = this.calcPos(this.currentScrollX, s.position.x, this.wholewidth, this.cardWidth);
-        s.position.y = this.calcPos(this.currentScrollY, s.position.y,this.wholeheight, this.cardHeight);
+        s.position.x = this.calcPos(this.currentScrollX, s.position.x, this.wholewidth, this.cardWidth, this.vMargin);
+        s.position.y = this.calcPos(this.currentScrollY, s.position.y,this.wholeheight, this.cardHeight, this.hMargin);
+        s.scale.x = this.scale * s.origScaleX;
+        s.scale.y = this.scale * s.origScaleY;
       });
   });
   }
